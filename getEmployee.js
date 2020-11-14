d03b0979e3f27f38083175b9c2fd370f457fc7df
ksg0102 <61391533+ksg0102@users.noreply.github.com>
2020-11-14 16:21:35 -0600

    Update getEmployee.js

--- getEmployee.js
+++ getEmployee.js
@@ -1,40 +1,61 @@
 'use strict';
 const AWS = require('aws-sdk');
 
+AWS.config.update({
+    region: "us-east-2"
+  });
 
 exports.handler = async (event, context) => {
 
-    var documentClient = new AWS.DynamoDB.DocumentClient();
-    let responseBody = "";
+    const documentClient = new AWS.DynamoDB.DocumentClient();
+   let responseBody = "";
     let statusCode = 0;
+    let sd = 0;
+    let id = "";
+    let passwd = "";
+    //let parsed =""
+    let inID = event.employeeID;
+    let inpas = event.employeepasswd;
+   
+    //const employeeID = event.pathParameters;
 
-    //const {employeeID} = event.pathParameters;
-
-    var params ={
+    const params ={
         TableName: "EmployeeDB",
         Key: {
-            "employeeID": event.employeeID
+            employeeID: event.employeeID
         }
     };
 
 
     try {
-        const data = await documentClient.delete(params).promise();
+        const data = await documentClient.get(params).promise();
         responseBody = JSON.stringify(data);
-        statusCode = 204;
+        id = JSON.stringify(data.Item.employeeID);
+        id = JSON.parse(id);
+        passwd = JSON.stringify(data.Item.employeepasswd);
+        passwd = JSON.parse(passwd);
+        
+        if(inID == id && inpas == passwd){
+           statusCode = 204; 
+        }
+        //parsed = JSON.parse(responseBody);
     } catch (err) {
-        responseBody = `Unable to delete employee: ${err}`;
-        statusCode = 403;
+        responseBody = `Unable to get employee: ${err}`;
+        statusCode = 404;
     }
+    
 
     const response = {
         statusCode: statusCode,
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
+            "access-control-allow-origin" : "*"
         },
-        body: responseBody
+        body: {
+            responseBody
+        }
     };
-
-    return response;
+    
+   return response;
 
 };
