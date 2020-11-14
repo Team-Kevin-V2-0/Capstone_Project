1464ba92f0ce28cd6c0639a719e34c27fd0480a7
corgan24 <61397410+corgan24@users.noreply.github.com>
2020-11-14 12:44:34 -0600

    Update deleteHD.js

--- deleteHD.js
+++ deleteHD.js
@@ -1,8 +1,45 @@
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+'use strict';
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+    region: "us-east-2"
+  });
+
+
+exports.handler = async (event, context) => {
+
+    var documentClient = new AWS.DynamoDB.DocumentClient();
+    let responseBody = "";
+    let statusCode = 0;
+
+    const {Simulator, SN} = JSON.parse(event.body);
+
+    var params ={
+        TableName: "Storage1",
+        Key: {
+            Simulator: Simulator,
+            SN: SN
+        }
     };
+
+
+    try {
+        const data = await documentClient.delete(params).promise();
+        responseBody = JSON.stringify(data);
+        statusCode = 204;
+    } catch (err) {
+        responseBody = `Unable to delete hard drive: ${err}`;
+        statusCode = 403;
+    }
+
+    const response = {
+        statusCode: statusCode,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: responseBody
+    };
+
     return response;
+
 };
