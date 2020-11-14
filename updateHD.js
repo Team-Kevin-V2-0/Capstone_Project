dc01a19a5b3318a52dde38275fb6d8114649d951
ksg0102 <61391533+ksg0102@users.noreply.github.com>
2020-11-14 16:03:04 -0600

    Update updateHD.js

--- updateHD.js
+++ updateHD.js
@@ -1,8 +1,53 @@
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+'use strict';
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+    region: "us-east-2"
+  });
+
+exports.handler = async (event, context) => {
+
+    const documentClient = new AWS.DynamoDB.DocumentClient();
+    let responseBody = "";
+    let statusCode = 0;
+
+    const {Simulator, SN, Computer, Cyc, Size, Typ} = JSON.parse(event.body);
+
+    const params = {
+        TableName: "Storage1",
+        Key: {
+            Simulator: Simulator,
+            SN: SN
+        },
+        
+        UpdateExpression: "set Info.Computer = :y, Info.Cyc = :e, Info.Size = :p, Info.Typ = :r",
+        ExpressionAttributeValues:{
+            ":y" : Computer,
+            ":e" : Cyc,
+            ":p" : Size,
+            ":r" : Typ
+        },
+        ReturnValues: "UPDATED_NEW"
     };
+
+
+    try {
+        const data = await documentClient.update(params).promise();
+        responseBody = JSON.stringify(data);
+        statusCode = 203;
+    } catch (err) {
+        responseBody = `Unable to Update storage: ${err}`;
+        statusCode = 403;
+    }
+
+    const response = {
+        statusCode: statusCode,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: responseBody
+    };
+
     return response;
+
 };
